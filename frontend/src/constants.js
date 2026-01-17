a164e9617d434a17e887723d5fb8e9f75dd598d6
Hari-Kato <hari.s@katomaran.com>
2026-01-17 22:07:16 +0530

    Enhance Firebase deployment workflow and update frontend configuration

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,5 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ?
-'https://proshop-backend-brown.vercel.app' : '';
-// export const BASE_URL = '';
+// Use the backend URL for both development and production
+export const BASE_URL = 'https://proshop-backend-brown.vercel.app';
  export const PRODUCTS_URL = '/api/products';
  export const USERS_URL = '/api/users';
  export const ORDERS_URL = '/api/orders';
